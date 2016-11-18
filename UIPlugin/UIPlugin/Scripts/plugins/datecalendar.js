0fc0680ea1716d57419f0a27180d7adf4d11ee37
albert samigullin <ar.smglln@gmail.com>
2016-11-18 11:06:10 +0300

    Implemented date selection, date parsing, multiple using widget

--- UIPlugin/UIPlugin/Scripts/plugins/datecalendar.js
+++ UIPlugin/UIPlugin/Scripts/plugins/datecalendar.js
@@ -1,15 +1,22 @@
 ﻿(function ($) {
     $.widget("ui.datecalendar", {
-        initialyear: 1995,
-        selectedMonthYear: {
-            Month: -1,
-            Year:-1
-        },
-        arraytd:{},
         options:
             {
                 delimeter: '/',
-                dateformat: 'ddmmyyyy'
+                dateformat: 'yyyyddmm',
+                _ids: {
+                    mainDivId: "",
+                    spanMonthYearDivId: "",
+                    leftButtonId: "",
+                    rightButtonId: "",
+                    tableOfDaysId: ""
+                },
+                _initialyear: 1995,
+                _selectedMonthYear: {
+                    Month: -1,
+                    Year:-1
+                },
+                _arraytd:{}
             },
         _create: function () {
             debugger;
@@ -18,15 +25,24 @@
             var dateformat = self.options.dateformat;
             checkValidDateFormat(dateformat);
             checkValidDelimeter(delimeter);
-            if (this.element.is('input')) {
-                // maindiv
-                var maindiv = $('<div class="calendarmaindiv">\
+            
+            if (self.element.is('input')) {
+                if (!$(self.element).attr('id')) {
+                    throw 'datecalendar widget is assigned only on inputs elements with assigned id property';
+                }
+                var inputid = $(self.element).attr('id');
+                self.options._ids.mainDivId = "maindiv_" + inputid;
+                self.options._ids.spanMonthYearDivId = "spanMonthYear_" + inputid;
+                self.options._ids.leftButtonId = "leftButtonId_" + inputid;
+                self.options._ids.rightButtonId = "rightButtonId_" + inputid;
+                self.options._ids.tableOfDaysId = "tableOfDaysId_" + inputid;
+                var maindiv = $('<div id="' + self.options._ids.mainDivId + '" class="calendarmaindiv">\
                                 <div class="monthyearname">\
-                                <input class="buttonleft" type="button" ></input>\
-                                <span class="monthname" id="monthname"></span>\
-                                <input class="buttonright" type="button"></input>\
+                                <input id="' + self.options._ids.leftButtonId + '" class="buttonleft" type="button" ></input>\
+                                <span class="monthname" id="' + self.options._ids.spanMonthYearDivId + '"id="monthname"></span>\
+                                <input id="' + self.options._ids.rightButtonId + '" class="buttonright" type="button"></input>\
                                 </div>\
-                                <table class="daysofweek">\
+                                <table id="' + self.options._ids.tableOfDaysId + '" class="daysofweek">\
                                 <tr>\
                                     <td class="daysofweek">Su</td>\
                                     <td class="daysofweek">Mo</td>\
@@ -97,24 +113,25 @@
                
                 // position input element
                 var inputPosition = this.element.position();
+                debugger;
                 // configure position div based on position input
-                $(".calendarmaindiv").css("top", inputPosition.top + this.element.outerHeight());
-                $(".calendarmaindiv").css("left", inputPosition.left - (maindiv.outerWidth() - this.element.outerWidth()));
-                $('.buttonright')
+                $("#"+self.options._ids.mainDivId).css("top", inputPosition.top + this.element.outerHeight());
+                $("#" + self.options._ids.mainDivId).css("left", inputPosition.left - (maindiv.outerWidth() - this.element.outerWidth()));
+                $('#' + self.options._ids.rightButtonId)
                     .on('click',
                     function () {
-                        var curDate = new Date(self.selectedMonthYear.Year, self.selectedMonthYear.Month, 1);
+                        var curDate = new Date(self.options._selectedMonthYear.Year, self.options._selectedMonthYear.Month, 1);
                         curDate.setMonth(curDate.getMonth() + 1);
-                        setCurrentDate(self.initialyear, self.selectedMonthYear, curDate, self.arraytd);
+                        setCurrentDate(self.options._initialyear, self, curDate);
                     });
-                $('.buttonleft')
+                $('#'+self.options._ids.leftButtonId)
                     .on('click',
                     function () {
-                        var curDate = new Date(self.selectedMonthYear.Year, self.selectedMonthYear.Month, 1);
+                        var curDate = new Date(self.options._selectedMonthYear.Year, self.options._selectedMonthYear.Month, 1);
                         curDate.setMonth(curDate.getMonth() - 1);
-                        setCurrentDate(self.initialyear, self.selectedMonthYear, curDate, self.arraytd);
+                        setCurrentDate(self.options._initialyear, self, curDate);
                     });
-                self.arraytd = convertTableToArray();
+                self.options._arraytd = convertTableToArray(self);
                 // initially div hidde
                 maindiv.hide();
 
@@ -123,11 +140,51 @@
                         
                         maindiv.show();
                         var input = event.target;
-
+                        debugger;
                         var neededDate;
                         if (input.value) {
                             try {
-                                 neededDate = Date.parseExact(input.value, [getDateMask(dateformat, delimeter)]);
+                                debugger;
+                                var datearr = input.value.split(self.options.delimeter);
+                                if (datearr.length !== 3) throw 'exception';
+                                var year="";
+                                var month="";
+                                var day="";
+
+                                if (dateformat.toLowerCase() === "ddmmyyyy") {
+                                    day = +datearr[0];
+                                    month=+datearr[1];
+                                    year = +datearr[2];
+                                }
+                                if (dateformat.toLowerCase() === "mmddyyyy") {
+                                    day = +datearr[1];
+                                    month = +datearr[0];
+                                    year = +datearr[2];
+                                }
+                                if (dateformat.toLowerCase() === "yyyymmdd") {
+                                    day = +datearr[2];
+                                    month = +datearr[1];
+                                    year = +datearr[0];
+                                }
+                                if (dateformat.toLowerCase() === "yyyyddmm") {
+                                    day = +datearr[1];
+                                    month = +datearr[2];
+                                    year = +datearr[0];
+                                }
+
+                                if (!Number.isInteger(day) || !Number.isInteger(month) || !Number.isInteger(year)) {
+                                    throw 'exception';
+                                }
+                                if (year < 1900 || year > 2100) {
+                                    throw 'exception';
+                                }
+                                if (month < 1 || month > 12) {
+                                    throw 'exception';
+                                }
+                                if (day<1 || day > daysInMonth(month - 1, year)) {
+                                    throw 'exception';
+                                }
+                                neededDate = new Date(year,month - 1, day);//Date.parseExact(input.value, [getDateMask(dateformat, delimeter)]);
                             } catch (ex) {
 
                                 neededDate = new Date();
@@ -135,7 +192,7 @@
                         } else {
                             neededDate = new Date();
                         }
-                        setCurrentDate(self.initialyear, self.selectedMonthYear, neededDate, self.arraytd);
+                        setCurrentDate(self.options._initialyear, self, neededDate);
                     });
             } else {
                 throw 'datecalendar widget only works on input elements';
@@ -152,6 +209,7 @@
 
 
     });
+
     var convertNumMonthToText= function (monthnum)
     {
         switch (monthnum)
@@ -203,13 +261,14 @@
         }
     }
 
-    var setCurrentDate = function (initialyear, selectedMonthYear, neededDate, tablearray) {
+    var setCurrentDate = function (initialyear, self, neededDate) {
 
         var curday = neededDate.getDate();
         var curmonth = neededDate.getMonth();
         var curyear = neededDate.getFullYear();
-        selectedMonthYear.Year = curyear;
-        selectedMonthYear.Month = curmonth;
+        self.options._selectedMonthYear.Year = curyear;
+        self.options._selectedMonthYear.Month = curmonth;
+        var tablearray = self.options._arraytd;
         var difyear = curyear - initialyear;
         // leap year one in 4 year
         var deviding;
@@ -236,13 +295,17 @@
         var k=0;
         var daysInCurMonth = daysInMonth(curmonth, curyear);
         debugger;
-        $("#monthname").html(convertNumMonthToText(curmonth) + " " + curyear);
+        $("#"+self.options._ids.spanMonthYearDivId).html(convertNumMonthToText(curmonth) + " " + curyear);
         clearTable(tablearray);
-        for (i = 1; i < tablearray.length; i++)
-        {
+        for (i = 0; i < tablearray.length; i++) {
+            debugger;
             for (; j < tablearray[i].length; j++) {
                 k++;
                 $(tablearray[i][j]).html(k);
+                $(tablearray[i][j]).addClass('numbertd');
+                if (k === curday) {
+                    $(tablearray[i][j]).addClass('selectedday');
+                }
                 if (k >= daysInCurMonth) return;
                 
             }
@@ -251,17 +314,44 @@
 
     }
 
-   function convertTableToArray () {
-        debugger;
+   function convertTableToArray (self) {
+        
         var tableArray = [];
-        $(".daysofweek tr").each(function (index) {
+        $("#"+self.options._ids.tableOfDaysId+" tr").each(function (index) {
             if (index==0) return;
             var arrayOfThisRow = [];
             var tableData = $(this).find('td');
             if (tableData.length > 0) {
                 tableData.each(function () {
-                    $(this).addClass('numbertd');
-                    arrayOfThisRow.push($(this));
+                    var td = $(this);
+                    td.on('click',
+                            function () {
+                                if (!td.html()) return;
+                                var value="";
+                                var dateformat = self.options.dateformat.toLowerCase();
+                                var delimeter = self.options.delimeter;
+                                var day = td.html();
+                                day = day.length > 1 ? day : "0" + day;
+                                var month = self.options._selectedMonthYear.Month + 1;
+                                month = month > 9 ? month : "0" + month;
+                                var year = self.options._selectedMonthYear.Year;
+                                if (dateformat === "ddmmyyyy") {
+                                    value= day + delimeter + month + delimeter + year;
+                                }
+                                if (dateformat === "mmddyyyy") {
+                                    value= month + delimeter + day + delimeter + year;
+                                }
+                                if (dateformat === "yyyymmdd") {
+                                    value= year + delimeter + month + delimeter + day;
+                                }
+                                if (dateformat=== "yyyyddmm") {
+                                    value= year + delimeter + day + delimeter + month;
+                                }
+                                self.element.val(value);
+                                debugger;
+                                $("#"+self.options._ids.mainDivId).hide();
+                            });
+                    arrayOfThisRow.push(td);
                 });
                 tableArray.push(arrayOfThisRow);
             }
@@ -271,14 +361,14 @@
     }
 
     function daysInMonth(month, year) {
-        return new Date(year, month, 0).getDate();
+        return new Date(year, month+1, 0).getDate();
     }
 
     var getSumDaysTillMonth = function (year, tillmonth) {
         debugger;
         var totalday = 0;
         var month;
-        for (month = 1; month < tillmonth+1; month++) {
+        for (month = 0; month < tillmonth; month++) {
             totalday += daysInMonth(month, year);
         }
         return totalday;
@@ -288,8 +378,10 @@
         if (!arraytd) return;
         var j;
         var i;
-        for (i = 1; i < arraytd.length; i++) {
-            for (j=0; j < arraytd[i].length; j++) {
+        for (i = 0; i < arraytd.length; i++) {
+            for (j = 0; j < arraytd[i].length; j++) {
+                $(arraytd[i][j]).removeClass('numbertd');
+                $(arraytd[i][j]).removeClass('selectedday');
                 $(arraytd[i][j]).html("");
             }
         }
